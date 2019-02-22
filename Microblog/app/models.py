d85b03fcd8b584a1180bf836784b99929e01c683
Adam <adelionek@gmail.com>
2019-02-22 19:22:55 +0100

    dodanie migracji

---
+++ Microblog/app/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+# user class inhereits from db.Model
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+# repr tells Python how to print obj of this class
+
