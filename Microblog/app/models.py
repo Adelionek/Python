9f281d86cd1c7a1fc8451b76ed7f9e059a3d3722
Adam <adelionek@gmail.com>
2019-02-23 18:33:55 +0100

    added post table

--- Microblog/app/models.py
+++ Microblog/app/models.py
@@ -1,6 +1,9 @@
 from app import db
+from datetime import datetime
 
 # user class inhereits from db.Model
+
+# snake case, so User will be named user. Can add attribute __tablename_
 
 
 class User(db.Model):
@@ -8,6 +11,8 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+# one to mny reltion. posts variable by 'one' side. 1st argument is 'many' table
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -15,3 +20,12 @@ class User(db.Model):
 
 # repr tells Python how to print obj of this class
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post{}>'.format(self.body)
