69bdb4debde024e8e6c27bb59f4db72d34b1921e
Adam <adelionek@gmail.com>
2019-02-24 16:25:40 +0100

    pull

--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,flash, redirect, url_for
 from app import app
-
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,4 +22,16 @@ def index():
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+# name of the view function, argument for url_for() func
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remembe_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sing in', form=form)
 
