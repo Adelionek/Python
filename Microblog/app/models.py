be7c38baaa0a6c1597c17290cb62a69629aca7b0
Adam <adelionek@gmail.com>
2019-03-02 19:33:12 +0100

    dodanie last seen

--- Microblog/app/models.py
+++ Microblog/app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 # user class inheritate from db.Model
 
@@ -15,6 +16,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 # one to many relation. posts variable by 'one' side. 1st argument is 'many' table
 # author - we will define an author in each added post
@@ -29,6 +32,10 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,7 +48,7 @@ class Post(db.Model):
         return '<Post {}>'.format(self.body)
 
 
-@login.user_loader
+@login.user_loader # callback for current_user
 def load_user(id):
-    return User.query.get(int(id))
+    return User.query.get(int(id))  # puts the user in db session
 
--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/')
@@ -74,3 +75,21 @@ def register():
         flash('Congratulations, you have registered successfully ')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')  # Dynamic component
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404() # returns a db object
+    posts = [
+        {'author': user, 'body': "Test post 1"},
+        {'author': user, 'body': "Test post 2"}
+    ]
+    return render_template('user.html', title='User', posts=posts, user=user)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
