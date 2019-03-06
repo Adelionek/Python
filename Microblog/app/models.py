4086ca15e37c30a1c770a9442e8dd90172292bca
Adam <adelionek@gmail.com>
2019-03-06 19:02:57 +0100

    db with followers

--- Microblog/app/models.py
+++ Microblog/app/models.py
@@ -9,6 +9,12 @@ from hashlib import md5
 
 # snake case, so User will be named user. Can add attribute __tablename_
 
+followers = db.Table('followers',
+    db.Column('follower_id', db. Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db. Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -17,6 +23,11 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     # one to many relation. posts variable by 'one' side. 1st argument is 'many' table
     # author - we will define an author in each added post
@@ -35,6 +46,17 @@ class User(db.Model, UserMixin):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
